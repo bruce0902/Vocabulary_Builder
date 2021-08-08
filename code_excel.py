d20e6fccbd32772373bd1279630c479d082d5051
Bruce <48899125+bruce0902@users.noreply.github.com>
2021-08-08 19:55:57 +0800

    v 1.6.0
    refactor the whole code to make it easier to read

--- code_excel.py
+++ code_excel.py
@@ -3,63 +3,13 @@ import os
 import keyboard
 import xlrd3 as xlrd
 
-
+num = 0
 def clear():
     os.system('cls')
     size = os.get_terminal_size()
     vertical = int((size[1])/2-2)
     for i in range(0, vertical):
         print()
-
-
-clear()
-
-
-mode = input(
-    "Press \'l\' to start learn mode, press \'t\' to start test mode: ").lower()
-while (mode != 'l' and mode != 't'):
-    clear()
-    print("wrong input! please input letter \'l\' or \'t\' ")
-    mode = input(
-        "Press \'l\' to start learn mode, press \'t\' to start test mode: ").lower()
-
-try:
-    with open("./group.json", "r") as r:
-        last_group = r.read()
-        print("The last group you learnt was group: " + last_group)
-except:
-    print("This is the first time you use this program")
-
-book = xlrd.open_workbook("data.xlsx")
-sheet = book.sheet_by_index(0)
-
-words = [str(sheet.cell_value(i, 0)) for i in range(1, sheet.nrows)]
-meaning = [str(sheet.cell_value(i, 1)) for i in range(1, sheet.nrows)]
-
-for i in range(len(words)):
-    meaning[i] = meaning[i].replace(' ', '')
-    meaning[i] = meaning[i].replace('\n', ' ').replace('\r', '')
-
-if len(words) % 50 == 0:
-    number_of_lists = int(len(words)/50)
-else:
-    number_of_lists = int(1+len(words)/50)
-for i in range(number_of_lists):
-    exec("words%s=words[50*%d:50*%d+50]" % (i+1, i, i))
-    exec("meaning%s=meaning[50*%d:50*%d+50]" % (i+1, i, i))
-
-group = input("There are " + str(number_of_lists) +
-              ''' groups, put in the group you want to learn,
-input 0 if you want to learn the saved files ''')
-lists = []
-for i in range(0, number_of_lists):
-    lists.append(str(i))
-
-while group not in lists:
-    clear()
-    print("wrong input! please input number like 1, 2, 3, 11...")
-    group = input(''' groups, put in the group you want to learn: 
-input 0 if you want to learn the saved files ''')
 
 
 def read_saved(location):
@@ -75,27 +25,6 @@ def read_saved(location):
     return cab_f
 
 
-if group == "0":
-    words = read_saved("saved_words.txt")
-    meaning = read_saved("saved_meaing.txt")
-else:
-    word_chosen = "words" + group
-    meaning_chosen = "meaning" + group
-
-    words = eval(word_chosen)
-    meaning = eval(meaning_chosen)
-
-
-clear()
-
-
-num = 0
-clear()
-
-with open("./group.json", "w") as w:
-    w.write(group)
-
-
 def print_middle(x):
     size = os.get_terminal_size()
     width_ori = len(x)
@@ -108,7 +37,7 @@ def print_middle(x):
 
 
 def learn(x):
-    global num, mode
+    global num
     if x.event_type == 'down' and x.name == 'left':
         clear()
         if num > 0:
@@ -237,17 +166,122 @@ def test(x):
         print_middle("saved!")
 
 
-if mode == "l":
-    print_middle(words[num])
-    print_middle(meaning[num])
-    keyboard.hook(learn)
+def show_last_group():
+    try:
+        with open("./group.json", "r") as r:
+            last_group = r.read()
+            print("The last group you learned was group: " + last_group)
+    except:
+        print("This is the first time you use this program")
 
-if mode == "t":
-    print_middle(words[num])
-    keyboard.hook(test)
-else:
-    pass
 
-keyboard.wait('ctrl')
+def choose_mode():
+    clear()
+    mode = input(
+        "Press \'l\' to start learn mode, press \'t\' to start test mode: ").lower()
+    while (mode != 'l' and mode != 't'):
+        clear()
+        print("wrong input! please input letter \'l\' or \'t\' ")
+        mode = input(
+            "Press \'l\' to start learn mode, press \'t\' to start test mode: ").lower()
+    show_last_group()
+    return mode
 
-clear()
+
+def write_last_group():
+    clear()
+    with open("./group.json", "w") as w:
+        w.write(group)
+
+
+def read_words():
+    book = xlrd.open_workbook("data.xlsx")
+    sheet = book.sheet_by_index(0)
+    words = [str(sheet.cell_value(i, 0)) for i in range(1, sheet.nrows)]
+    return words
+
+
+def read_meaning():
+    book = xlrd.open_workbook("data.xlsx")
+    sheet = book.sheet_by_index(0)
+    meaning = [str(sheet.cell_value(i, 1)) for i in range(1, sheet.nrows)]
+    for i in range(len(all_words)):
+        meaning[i] = meaning[i].replace(' ', '')
+        meaning[i] = meaning[i].replace('\n', ' ').replace('\r', '')
+    return meaning
+
+
+def return_num_of_lists():
+    if len(all_words) % 50 == 0:
+        number_of_lists = int(len(all_words)/50)
+    else:
+        number_of_lists = int(1+len(all_words)/50)
+        return number_of_lists
+
+
+def choose_group():
+    group = input("There are " + str(num_of_lists) +
+                  ''' groups, put in the group you want to learn,
+    input 0 if you want to learn the saved files ''')
+    lists = []
+    for i in range(0, num_of_lists + 1):
+        lists.append(str(i))
+
+    while group not in lists:
+        clear()
+        print("wrong input! please input number like 1, 2, 3, 11...")
+        group = input(''' groups, put in the group you want to learn: 
+    input 0 if you want to learn the saved files ''')
+    return group
+
+
+def main_program(mode):
+    if mode == "l":
+        print_middle(words[0])
+        print_middle(meaning[0])
+        keyboard.hook(learn)
+
+    if mode == "t":
+        print_middle(words[0])
+        keyboard.hook(test)
+    else:
+        pass
+    keyboard.wait('ctrl')
+    clear()
+
+
+def return_words(group):
+    if group == "0":
+        words = read_saved("saved_words.txt")
+    else:
+        words = eval("words" + group)
+    return words
+
+
+def return_meaning(group):
+    if group == "0":
+        meaning = read_saved("saved_meaing.txt")
+    else:
+        meaning = eval("meaning" + group)
+    return meaning
+
+
+if __name__ == "__main__":
+    mode = choose_mode()
+    all_words = read_words()
+    all_meaning = read_meaning()
+    num_of_lists = return_num_of_lists()
+
+    for i in range(num_of_lists):
+        exec("words%s=all_words[50*%d:50*%d+50]" % (i+1, i, i))
+        exec("meaning%s=all_meaning[50*%d:50*%d+50]" % (i+1, i, i))
+
+    group = choose_group()
+
+    words = return_words(group)
+    meaning = return_meaning(group)
+
+    num = 0
+    write_last_group()
+    main_program(mode)
+
